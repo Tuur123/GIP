d11bc78497169162355d8d7d16ff7ee4f1588106
Tuur123 <43994001+Tuur123@users.noreply.github.com>
2019-01-08 21:43:30 +0100

    methodes database

--- Website/Website_GIP/Database.cs
+++ Website/Website_GIP/Database.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using System.Text;
+using System.Data.OleDb;
+using System.Security.Cryptography;
 
-public class Class1
+public class Database
 {
-	public Class1()
+    private string conString = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = ..\Database.accdb; Persist Security Info = False;";
+
+    public Database()
 	{
+
 	}
+
+    public void AddUser(string name, string password)
+    {
+
+    }
+
+    public void AddData(string co2, string temperatuur, string locatie, string vochtigheid)
+    {
+
+    }
 }
