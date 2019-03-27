d1cc96864fe19435538a87bc9008570dddf0c022
Tuur123 <43994001+Tuur123@users.noreply.github.com>
2019-03-27 11:52:19 +0100

    markers werkennn
    ye boii

--- Website/Website_GIP/Website_GIP/map.js
+++ Website/Website_GIP/Website_GIP/map.js
@@ -2,8 +2,6 @@
 
     mapboxgl.accessToken = 'pk.eyJ1IjoicnViZW5hcnRodXIiLCJhIjoiY2pycXR6dnljMGJ3NDN5cGV5dGRlOWpqcSJ9.IeEQqzqf_kgs8J3rj2U5xw';
 
-    var json;
-
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v9',
@@ -11,28 +9,26 @@
         zoom: 13
     });
 
-    PageMethods.GetDataFromDB(OnSuccess);
+    PageMethods.GetDataFromDB(onSuccess);
 
-    function OnSuccess(response) {
+    function onSuccess(response) {
+
         console.log(response);
-        var data_parsed = JSON.parse(data.responseText);
-        console.log(data_parsed);
-        json = data_parsed;
+
+        // add markers to map
+        JSON.parse(response).features.forEach(function (marker) {
+            // create a HTML element for each feature
+            var el = document.createElement('div');
+            el.className = 'marker';
+
+            // make a marker for each feature and add to the map
+            new mapboxgl.Marker(el)
+                .setLngLat(marker.geometry.coordinates)
+                .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+                    .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+                .addTo(map);
+        });
+
+        map.addControl(new mapboxgl.NavigationControl());
     }
-
-    // add markers to map
-    json.features.forEach(function (marker) {
-        // create a HTML element for each feature
-        var el = document.createElement('div');
-        el.className = 'marker';
-
-        // make a marker for each feature and add to the map
-        new mapboxgl.Marker(el)
-            .setLngLat(marker.geometry.coordinates)
-            .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-                .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
-            .addTo(map);
-    });
-
-    map.addControl(new mapboxgl.NavigationControl());
 });
